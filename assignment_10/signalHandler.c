b1669bc01095cc6e2ff479250cb4a67499827b7b
Samanyu 3482 <gautamsamanyu3482@gmail.com>
2026-09-23 11:48:41 +0530

    signal file assignment 10

---
+++ assignment_10/signalHandler.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+volatile sig_atomic_t signal_count = 0;
+
+void handle_sigint(int sig) {
+    signal_count++;
+    printf("\nCtrl+C pressed %d time(s)\n", signal_count);
+
+    if (signal_count == 3) {
+        printf("Exiting...\n");
+        exit(0);
+    }
+}
+
+int main() {
+    signal(SIGINT, handle_sigint);
+
+    printf("Program running (PID: %d). Try pressing Ctrl+C...\n", getpid());
+
+    while (1) {
+        sleep(1);
+    }
+
+    return 0;
+}
